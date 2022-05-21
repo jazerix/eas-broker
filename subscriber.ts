fe4a51ddc56cd8f378dac24fa8d04a7d31ac62bf
Niels Faurskov <niels@faurskov.dev>
2022-05-21 14:53:02 +0200

    init

---
+++ subscriber.ts
@@ -0,0 +1,18 @@
+import * as mqtt from "mqtt";
+import { MongoClient } from "mongodb";
+const client = mqtt.connect("mqtt://127.0.0.1:1883");
+
+console.log("HELLO");
+client.on("connect", function () {
+  console.log("HELLO");
+  client.subscribe("presence", function (err) {
+    if (!err) {
+      client.publish("presence", "Hello mqtt");
+    }
+  });
+});
+
+client.on("message", function (topic, message) {
+  // message is Buffer
+  console.log(message.toString());
+});
